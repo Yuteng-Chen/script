7ce3802f0c9cc080c09db4847111364988f3619f
hyq-csm <2402227288@qq.com>
2026-02-02 08:44:23 +0000

    v1

--- train_stage_1.sh
+++ train_stage_1.sh
@@ -1,5 +1,5 @@
 DATETIME=$(date '+%Y-%m-%d-%H')
-RUN_NAME="check600_v_1_video-r1_stage1_test_cyt"
+RUN_NAME="check_1887_v9_valid_tool"
 OUTPUT_DIR=/scratch/prj0000000262-bucket/ocr/ec/TimeSearch-R_latest/experiment/$RUN_NAME/$DATETIME
 mkdir -p $OUTPUT_DIR
 export WANDB_PROJECT=timesearch-R-stage_1
@@ -40,7 +40,7 @@ torchrun --nproc_per_node=${NUM_GPUS} --nnodes=1 --node_rank=0 \
     --model_name_or_path $MODEL_BASE \
     --train_data_path $TRAIN_PATH \
     --video_folder $VIDEO_ROOT \
-    --reward_func v9 \
+    --reward_func v9_valid_tool \
     --prompt_template v3 \
     --tool_name_list seek_video_frames \
     --max_interaction_turns 4 \
