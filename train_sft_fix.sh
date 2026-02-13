5af5fc4acdee590b856cce6051226596e37a1b73
chensm <2402227288@qq.com>
2026-02-13 18:36:10 +0800

    group

--- train_sft_fix.sh
+++ train_sft_fix.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
 DATETIME=$(date '+%Y-%m-%d-%H')
-RUN_NAME="SFT_Video_R1_cyt_60_frame_epoch1_zero2_fix_work1——pilance-16-128" ## 必须，不然OOM
+RUN_NAME="SFT_Video_R1_cyt_60_frame_epoch1_zero2_fix_work1——pilance-16-128-group-length" ## 必须，不然OOM
 OUTPUT_DIR=/scratch/prj0000000262-bucket/ocr/ec/TimeSearch-R_latest/experiment/$RUN_NAME/$DATETIME
 mkdir -p $OUTPUT_DIR
 
+# # === 环境变量设置 (关键修改部分) ===
+
+# # 1. [核心修复] 指定用于握手的网卡
+# # 根据你的 ifconfig，bond0.3102 是主通信网卡
+# export NCCL_SOCKET_IFNAME=bond0.3102
+
+# # 2. [推荐] 单机训练禁用 IB，防止 IB 驱动报错干扰 NCCL 初始化
+# export NCCL_IB_DISABLE=1
+
+# # 3. [可选] P2P 设置
+# # 如果你的显卡之间有 NVLink 桥接，请保留下面这行 NVL 设置，并注释掉 DISABLE
+# export NCCL_P2P_LEVEL=NVL
+# # export NCCL_P2P_DISABLE=1  # 除非遇到 P2P 报错或卡死，否则建议注释掉这行以获得更好性能
+
+# # 4. 调试日志 (如果再次报错，取消下面两行的注释看详细信息)
+# # export NCCL_DEBUG=INFO
+# # export TORCH_DISTRIBUTED_DEBUG=DETAIL
+
+# # 牺牲速度换稳定性
+# export NCCL_P2P_DISABLE=1
+# export NCCL_IB_DISABLE=1
+# export NCCL_SOCKET_IFNAME=bond0.3102
 
 # === 其他常规设置 ===
 export WANDB_PROJECT=TimeSearch-R-SFT
@@ -29,7 +51,7 @@ VIDEO_ROOT=/xuhongbo/shuimu.chen/LongVideoBench/videos_480p_noaudio
 MODEL_BASE=/scratch/prj0000000262-bucket/ocr/ec/models/Qwen2.5-VL-7B-Instruct
 
 # 启动命令
-accelerate launch --nproc_per_node=${NUM_GPUS} --nnodes=1 --node_rank=0 \
+torchrun --nproc_per_node=${NUM_GPUS} --nnodes=1 --node_rank=0 \
     --master_addr=localhost --master_port=${MASTER_PORT} \
     time_r1/sft.py \
     --deepspeed /scratch/prj0000000262-bucket/ocr/ec/TimeSearch-R_latest/scripts/zero2_offload.json \
@@ -56,6 +78,6 @@ accelerate launch --nproc_per_node=${NUM_GPUS} --nnodes=1 --node_rank=0 \
     --num_train_epochs 1 \
     --run_name $RUN_NAME \
     --report_to wandb \
-    --save_steps 1000 \
     --group_by_length True \
+    --save_steps 1000 \
     --save_only_model true
